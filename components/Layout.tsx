8adc39aeff3f59cd919a4f26c807bb55b2a15acc
rebecsan <rebecca.sandelin@gmail.com>
2020-09-10 10:21:15 +0200

    Body width scale fluidly  to larger size

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { Footer } from "../components/Footer";
 
 const Layout: React.FC = ({ children }) => (
-  <div className="mx-auto flex flex-col min-h-screen md:w-2/3">
+  <div className="mx-auto flex flex-col min-h-screen max-w-5xl">
     <Head>
       <meta
         name="viewport"
