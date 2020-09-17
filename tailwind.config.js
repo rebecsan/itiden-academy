2d18d9beaa80532ee44b3522003325baeae4a226
rebecsan <rebecca.sandelin@gmail.com>
2020-09-17 23:59:01 +0200

    Add missing dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -48,7 +48,7 @@ module.exports = {
     // Divide contents of flex container into several columns
     require("tailwindcss-multi-column")(),
     // Add variants for darkmode
-    require("@danestves/tailwindcss-darkmode")(),
+    require("tailwindcss-dark-mode")(),
   ],
   future: {
     removeDeprecatedGapUtilities: true,
