55b3efa9c98c22a85eff71018baa2a8fc8aeeef1
rebecsan <rebecca.sandelin@gmail.com>
2020-09-18 11:39:37 +0200

    More work on darkmode

--- static/noflash.js
+++ static/noflash.js
@@ -1,3 +1,4 @@
+// Insert this script in your index.html right after the <body> tag.
 // This will help to prevent a flash if dark mode is the default.
 
 (function () {
@@ -11,6 +12,9 @@
     document.body.classList.remove(darkMode ? classNameLight : classNameDark);
   }
 
+  var preferDarkQuery = "(prefers-color-scheme: dark)";
+  var mql = window.matchMedia(preferDarkQuery);
+  var supportsColorSchemeQuery = mql.media === preferDarkQuery;
   var localStorageTheme = null;
   try {
     localStorageTheme = localStorage.getItem(storageKey);
@@ -24,10 +28,13 @@
   if (localStorageExists) {
     // source of truth from localStorage
     setClassOnDocumentBody(localStorageTheme);
+  } else if (supportsColorSchemeQuery) {
+    // source of truth from system
+    setClassOnDocumentBody(mql.matches);
+    localStorage.setItem(storageKey, mql.matches);
   } else {
     // source of truth from document.body
     var isDarkMode = document.body.classList.contains(classNameDark);
     localStorage.setItem(storageKey, JSON.stringify(isDarkMode));
   }
-  alert(123);
 })();
